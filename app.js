f8bbf25f7abc0ffb36e0daa1b4a8a7123068486e
Bonnie Hardie <bonniemariehardie@gmail.com>
2020-10-05 18:04:26 -0400

    added Users routes and seeder

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-// This is Back end
+// This is Back End
+const express = require('express');
+const app = express();
 
+const userRouter = require('./routes/users');
+
+app.use('/users', userRouter);
