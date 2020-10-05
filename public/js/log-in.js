eab79e512c7c8921a8a43e55f404177f9407adb4
Bonnie Hardie <bonniemariehardie@gmail.com>
2020-10-05 19:56:34 -0400

    almost complete log-in.js

---
+++ public/js/log-in.js
@@ -0,0 +1,36 @@
+const logInForm = document.querySelector('.log-in-form');
+logInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(logInForm);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const body = { email, password };
+
+    try{
+        const res = await fetch('http://localhost:8080/users/token', {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (!res.ok) {
+            throw res;
+        }
+        const {
+            token,
+            user: { id },
+
+        } = await res.json
+
+        localStorage.setItem('MEDIUM_ACCESS_TOKEN', token);
+        localStorage.setItem('USER_ID', id);
+        window.location.href = '/';
+
+    } catch(err) {
+        if(err.status >= 400 && err.status < 600) {
+            const errorJSON = await err.json();
+        }
+    }
+
+})
