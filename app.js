45c975f7e944b53e6a3e3e3395ac9b783ed1cfc4
alycialwang@gmail.com <alyciawang@Alycias-MacBook-Pro.local>
2020-10-05 19:05:38 -0400

    Added Body Parser

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // This is Back End
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 const userRouter = require("./routes/users");
 
+app.use(morgan("dev"));
+app.use(express.json());
 app.use("/users", userRouter);
 
 module.exports = app;
