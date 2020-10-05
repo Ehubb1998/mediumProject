4f0140c1336b80935b8432460b385acf5d10332b
alycialwang@gmail.com <alyciawang@Alycias-MacBook-Pro.local>
2020-10-05 19:20:52 -0400

    Authorization Working

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const userRouter = express.Router();
 const bcrypt = require("bcryptjs");
+const { getUserToken, requireAuth } = require("../auth");
 const { check } = require("express-validator");
 const { asyncHandler, handleValidationErrors } = require("../utils");
 const db = require("../db/models");
 const { User } = db;
+
+userRouter.use(requireAuth);
 
 const validateUserName = check("userName")
   .exists({ checkFalsy: true })
@@ -20,16 +23,25 @@ const validateEmailAndPassword = [
     .withMessage("Please provide a valid Email"),
 ];
 
+userRouter.get("/", async (req, res) => {
+  res.send("Hello!");
+});
+
 userRouter.post(
   "/",
   handleValidationErrors,
   validateUserName,
   validateEmailAndPassword,
   asyncHandler(async (req, res) => {
-    // user creation logic here
     const { userName, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ userName, email, hashedPassword });
+
+    const token = getUserToken(user);
+    res.status(201).json({
+      user: { id: user.id },
+      token,
+    });
   })
 );
 
