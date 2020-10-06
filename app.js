8a5685b5f2963a7cd14c7bea22c94f98838f1b10
Bonnie Hardie <bonniemariehardie@gmail.com>
2020-10-06 19:14:20 -0400

    started debugging with JM to fix auth and route issues

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", userRouter);
+app.use("/articles", articleRouter);
 
 app.set("view engine", "pug");
 
