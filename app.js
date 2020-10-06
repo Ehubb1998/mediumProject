1502cac4ba12132d1d9a580414a30b424be14368
Bonnie Hardie <bonniemariehardie@gmail.com>
2020-10-06 12:17:20 -0400

    deleted index.js

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 // This is Back End
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const userRouter = require("./routes/users");
 
 app.use("/users", userRouter);
+app.use(express.static(path.join(__dirname, "public")));
 
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+    res.render("create-user");
+})
 module.exports = app;
