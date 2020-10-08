e7f17013e690abca33f4539925638696ac195f50
Bonnie Hardie <bonniemariehardie@gmail.com>
2020-10-08 18:44:10 -0400

    resolved conflicts

--- db/models/user.js
+++ db/models/user.js
@@ -38,5 +38,11 @@ module.exports = (sequelize, DataTypes) => {
     })
   };
 
+  User.prototype.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.hashedPassword.toString());
+    // console.log(validation);
+  };
+  
   return User;
+
 };
