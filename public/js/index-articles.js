44a391a62d2a0a0a50026231dce8ba1f7265b6f3
John-Michael Riley <johnmichael.w.riley@gmail.com>
2020-10-09 21:57:10 -0400

    convert forEach to for loop in articleBox3

--- public/js/index-articles.js
+++ public/js/index-articles.js
@@ -1,10 +1,11 @@
 var content = {
-  init: async() => {
+  init: async () => {
     await content.mainArticle();
     await content.mainSideArticles();
     await content.trendingArticles();
     await content.articleBox3();
-    //await content.following();
+    content.following();
+    console.log(document.querySelectorAll('.followButton'));
   },
 
   mainArticle: async () => {
@@ -19,22 +20,18 @@ var content = {
 
       const author = await content.getUser(main.userId);
 
-      document.querySelector(".artA__main--title").innerHTML = main.title;
-      document.querySelector(".artA__main--author").innerHTML = author.userName;
-      document
-        .querySelector(".artA__main--author")
-        .appendChild(content.followButton(main.userId));
+      document.querySelector('.artA__main--title').innerHTML = main.title;
+      document.querySelector('.artA__main--author').innerHTML = author.userName;
+      document.querySelector('.artA__main--author').appendChild(content.followButton(main.userId));
 
-      document
-        .querySelector(".artA__main--link")
-        .setAttribute("href", `http://localhost:8080/articles/${id}`);
+      document.querySelector('.artA__main--link').setAttribute('href', `http://localhost:8080/articles/${id}`);
     } catch (err) {
       console.error(err);
     }
   },
 
   mainSideArticles: async () => {
-    const slots = document.querySelectorAll(".artA__side--Article");
+    const slots = document.querySelectorAll('.artA__side--Article');
     await slots.forEach(async (slot) => {
       try {
         const count = await content.articleCount();
@@ -44,19 +41,16 @@ var content = {
         const main = randomArticle.article;
         const author = await content.getUser(main.userId);
 
-        const divA = document.createElement("div");
-        const divM = document.createElement("div");
+        const divA = document.createElement('div');
+        const divM = document.createElement('div');
 
-        const name = document.createElement("h5");
+        const name = document.createElement('h5');
         name.textContent = author.userName;
-        const title = document.createElement("h3");
+        const title = document.createElement('h3');
         title.textContent = main.title;
-        const image = document.createElement("img");
-        image.setAttribute(
-          "src",
-          `https://picsum.photos/id/${content.randomNum(100)}/100/100`
-        );
-        image.setAttribute("alt", "sorry blind people.");
+        const image = document.createElement('img');
+        image.setAttribute('src', `https://picsum.photos/id/${content.randomNum(100)}/100/100`);
+        image.setAttribute('alt', 'sorry blind people.');
         const button = content.followButton(main.userId);
         // console.log(button);
         // button.addEventListener("click", () => content.following(id))
@@ -74,8 +68,8 @@ var content = {
   },
 
   trendingArticles: async () => {
-    const slots = document.querySelectorAll(".content__articleB");
-    slots.forEach(async (slot) => {
+    const slots = document.querySelectorAll('.content__articleB');
+    await slots.forEach(async (slot) => {
       try {
         const count = await content.articleCount();
         const id = await content.randomNum(count);
@@ -84,18 +78,15 @@ var content = {
         const main = randomArticle.article;
         const author = await content.getUser(main.userId);
 
-        const divA = document.createElement("div");
-        const divM = document.createElement("div");
+        const divA = document.createElement('div');
+        const divM = document.createElement('div');
 
-        const name = document.createElement("h5");
+        const name = document.createElement('h5');
         name.textContent = author.userName;
-        const title = document.createElement("h3");
+        const title = document.createElement('h3');
         title.textContent = main.title;
-        const image = document.createElement("img");
-        image.setAttribute(
-          "src",
-          `https://picsum.photos/id/${content.randomNum(100)}/80/80`
-        );
+        const image = document.createElement('img');
+        image.setAttribute('src', `https://picsum.photos/id/${content.randomNum(100)}/80/80`);
 
         name.appendChild(content.followButton(main.userId));
         slot.appendChild(divA);
@@ -111,8 +102,8 @@ var content = {
   },
 
   articleBox3: async () => {
-    const slots = document.querySelectorAll(".content__articleC--Article");
-    slots.forEach(async (slot) => {
+    const slots = document.querySelectorAll('.content__articleC--Article');
+    for (let slot of slots) {
       try {
         const count = await content.articleCount();
         const id = await content.randomNum(count);
@@ -121,18 +112,15 @@ var content = {
         const main = randomArticle.article;
         const author = await content.getUser(main.userId);
 
-        const divA = document.createElement("div");
-        const divM = document.createElement("div");
+        const divA = document.createElement('div');
+        const divM = document.createElement('div');
 
-        const name = document.createElement("h4");
+        const name = document.createElement('h4');
         name.textContent = author.userName;
-        const title = document.createElement("h2");
+        const title = document.createElement('h2');
         title.textContent = main.title;
-        const image = document.createElement("img");
-        image.setAttribute(
-          "src",
-          `https://picsum.photos/id/${content.randomNum(100)}/200/133`
-        );
+        const image = document.createElement('img');
+        image.setAttribute('src', `https://picsum.photos/id/${content.randomNum(100)}/200/133`);
 
         name.appendChild(content.followButton(main.userId));
         slot.appendChild(divA);
@@ -144,7 +132,38 @@ var content = {
       } catch (err) {
         console.error(err);
       }
-    });
+    }
+    // await slots.forEach(async (slot) => {
+    //   try {
+    //     const count = await content.articleCount();
+    //     const id = await content.randomNum(count);
+    //     const res = await fetch(`http://localhost:8080/articles/${id}`);
+    //     const randomArticle = await res.json();
+    //     const main = randomArticle.article;
+    //     const author = await content.getUser(main.userId);
+
+    //     const divA = document.createElement('div');
+    //     const divM = document.createElement('div');
+
+    //     const name = document.createElement('h4');
+    //     name.textContent = author.userName;
+    //     const title = document.createElement('h2');
+    //     title.textContent = main.title;
+    //     const image = document.createElement('img');
+    //     image.setAttribute('src', `https://picsum.photos/id/${content.randomNum(100)}/200/133`);
+
+    //     name.appendChild(content.followButton(main.userId));
+    //     slot.appendChild(divA);
+    //     slot.appendChild(divM);
+    //     divA.appendChild(name);
+    //     divA.appendChild(title);
+    //     divA.innerHTML += `<a href="http://localhost:8080/articles/${id}">Read More</a>`;
+    //     divM.appendChild(image);
+    //   } catch (err) {
+    //     console.error(err);
+    //   }
+    // });
+    console.log(document.querySelectorAll('.followButton'));
     //content.following();
   },
 
@@ -161,7 +180,7 @@ var content = {
 
   articleCount: async () => {
     try {
-      const res = await fetch("http://localhost:8080/articles");
+      const res = await fetch('http://localhost:8080/articles');
       const data = await res.json();
       const count = data.articles.length;
       return count;
@@ -180,47 +199,44 @@ var content = {
   },
 
   followButton: (id) => {
-    const button = document.createElement("button");
-    button.setAttribute("id", id);
-    button.classList.add("followButton");
-    button.textContent = "Follow";
-    //console.log("HELLO!!!!!!");
-    button.addEventListener("click", () => content.following(id))
+    const button = document.createElement('button');
+    button.setAttribute('id', id);
+    button.classList.add('followButton');
+    button.textContent = 'Follow';
+    // button.addEventListener('click', (e) => content.following(e, id));
+    // button.classList.add('test');
+    // console.dir(button);
     return button;
   },
 
-  following: async(authorId) => {
-    // const followButtons = document.querySelectorAll(".followbutton");
+  following: async () => {
+    const followButtons = document.querySelectorAll('.followbutton');
     // console.log(followButtons);
-    // followButtons.forEach((button) => {
-    //   button.addEventListener("click", async (e) => {
-      const userId = localStorage.getItem("MEDIUM_USER_ID");
-    //   const authorId = e.target.id
-      console.log(authorId);
+    followButtons.forEach((button) => {
+      button.addEventListener('click', async (e) => {
+        const userId = localStorage.getItem('MEDIUM_USER_ID');
+        const authorId = e.target.id;
+        console.log(authorId);
 
-      try {
-
-        const res = await fetch(`/users/${authorId}/addFollow`, {
-            method: "POST",
+        try {
+          const res = await fetch(`/users/${authorId}/addFollow`, {
+            method: 'POST',
             body: JSON.stringify({ userId, authorId }),
             headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${localStorage.getItem('MEDIUM_ACCESS_TOKEN')}`
-            }
-        });
-        if (!res.ok) {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${localStorage.getItem('MEDIUM_ACCESS_TOKEN')}`,
+            },
+          });
+          if (!res.ok) {
             throw res;
+          }
+          const data = await res.json();
+        } catch (e) {
+          console.log(e);
         }
-        const data = await res.json();
-
-      } catch(e) {
-          console.log(e)
-      }
-// })
-// });
-
-  }
-
+      });
+    });
+  },
 };
 
-document.addEventListener("DOMContentLoaded", async () => content.init());
+document.addEventListener('DOMContentLoaded', async () => content.init());
