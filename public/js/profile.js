8f49337e84bc2f783451f7c31e521ac868832f49
Beau Kenny <beau.kenny@yahoo.com>
2020-10-11 14:16:00 -0500

    branch traveling

---
+++ public/js/profile.js
@@ -0,0 +1,10 @@
+// const getFollowers = async () => {
+//   const followers = await fetch("/users/:id(\\d+)/followers");
+//   console.log(followers);
+//   console.log("THIS IS HERE!!!");
+// }
+
+// getFollowers();
+// const followButton = document.querySelector(".followButton");
+// followButton.addEventListener("click", () => following)
+console.log(userName);
