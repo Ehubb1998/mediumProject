25bbc0f4d9b54f73228e0d49b635c2dfb3076aa7
Beau Kenny <beau.kenny@yahoo.com>
2020-10-11 19:26:18 -0500

    profile page following complete

--- routes/users.js
+++ routes/users.js
@@ -134,6 +134,7 @@ userRouter.get("/profile/:id", asyncHandler(async (req, res, next) => {
     },
     include: 'Articles'
   });
+
   if (user) {
     res.render("profile-page", { user } );
   } else {
